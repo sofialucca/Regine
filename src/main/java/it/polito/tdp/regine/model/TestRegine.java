924a3201e8fafdbf4693da649e77c5a823bac783
Sofia Lucca <s257431@studenti.polito.it>
2021-04-12 10:18:07 +0200

    correzione

--- src/main/java/it/polito/tdp/regine/model/TestRegine.java
+++ src/main/java/it/polito/tdp/regine/model/TestRegine.java
@@ -8,7 +8,7 @@ public class TestRegine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Regine r =new Regine();
-		List<Integer>soluzione=new ArrayList<>();
+		List<List<Integer>>soluzione=new ArrayList<>();
 		soluzione=r.risolvi(6);
 		System.out.println(soluzione);
 	}
